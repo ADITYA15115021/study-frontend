48a02ee0f0e1001c5af35a8b8853220cc341cb9a
ADITYA KAMBOJ <sierra.phoenix1511@gmail.com>
2025-04-12 14:32:25 +0530

    added appbar-3

--- src/pages/QuizHistory.jsx
+++ src/pages/QuizHistory.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react"
-import { Spinner } from "../components/Spinner";
-import { Appbar } from "../components/AppBar";
+import { Spinner } from "../components/Spinner.jsx";
+import { Appbar } from "../components/AppBar.jsx";
 
 export default function QuizHistory(){
 
